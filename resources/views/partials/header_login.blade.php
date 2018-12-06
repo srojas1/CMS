3703c1f64d823af1815d59f0f0d4932feaf6be96
srojas1 <samuelro444@gmail.com>
2018-12-06 17:33:32 -0500

    add

--- resources/views/partials/header_login.blade.php
+++ resources/views/partials/header_login.blade.php
@@ -2,7 +2,7 @@
 <meta name="viewport" content="width=device-width, initial-scale=1">
 <meta name="Description" content="Administrador.">
 
-<link rel="stylesheet" type="text/css" href="{{ asset('assets/styles/style.css') }}">
 <link rel="stylesheet" type="text/css" href="{{ asset('assets/styles/bootstrap.min.css') }}">
+<link rel="stylesheet" type="text/css" href="{{ asset('assets/styles/style.css') }}">
 
 <script src="http://ajax.googleapis.com/ajax/libs/jquery/1.9.1/jquery.js"></script>
