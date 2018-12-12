cb7418ffde3fb2ec81edc4b584ab2065eed1561b
Samuel Rojas <srojas@belatrixsf.com>
2018-12-12 17:37:20 -0500

    add

--- resources/views/partials/header.blade.php
+++ resources/views/partials/header.blade.php
@@ -3,24 +3,21 @@
 <meta name="author" content="{{ Config::get('cms.author') }}">
 <meta name="viewport" content="width=device-width, initial-scale=1">
 
-<script src="http://ajax.googleapis.com/ajax/libs/jquery/1.9.1/jquery.js"></script>
 <link rel="stylesheet" type="text/css" href="//cdnjs.cloudflare.com/ajax/libs/font-awesome/4.3.0/css/font-awesome.min.css">
 <link rel="stylesheet" type="text/css" href="//cdnjs.cloudflare.com/ajax/libs/animate.css/3.2.3/animate.min.css">
 <link rel="stylesheet" type="text/css" href="{{ asset('assets/styles/cms-main.css') }}">
-
+<link rel="stylesheet" type="text/css" href="{{ asset('assets/styles/style.css') }}">
+<link rel="stylesheet" type="text/css" href="{{ asset('assets/styles/bootstrap.min.css') }}">
+<link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
 
 {{--<link rel="stylesheet" type="text/css" href="{{ asset('assets/styles/bootstrap.'.Config::get('theme.name', 'default').'.min.css') }}">--}}
 {{--<link rel="stylesheet" type="text/css" href="{{ asset('assets/styles/cms-custom.css') }}">--}}
 
 
-<link rel="stylesheet" type="text/css" href="{{ asset('assets/styles/style.css') }}">
-<link rel="stylesheet" type="text/css" href="{{ asset('assets/styles/bootstrap_.min.css') }}">
-<link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
+<script src="http://ajax.googleapis.com/ajax/libs/jquery/1.9.1/jquery.js"></script>
 <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
 <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.3/umd/popper.min.js" integrity="sha384-ZMP7rVo3mIykV+2+9J3UJ46jBk0WLaUAdn689aCwoqbBJiSnjAK/l8WvCWPIPm49" crossorigin="anonymous"></script>
 <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/js/bootstrap.min.js" integrity="sha384-ChfqqxuZUCnJSK3+MXmPNIyE6ZbWh2IMqE241rYiqJxyMiZ6OW/JmZQ5stwEULTy" crossorigin="anonymous"></script>
-@section('css')
-@show
 
 <!--[if lt IE 9]>
   <script type="text/javascript" src="//cdnjs.cloudflare.com/ajax/libs/html5shiv/3.7.2/html5shiv.min.js"></script>
